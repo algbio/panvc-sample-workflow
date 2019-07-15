b0f796303e585cf510c6267ab6ba24156147b8fe
Daniel Valenzuela <dvalenzu@cs.helsinki.fi>
2019-07-15 10:26:45 +0300

    moving files

---
+++ pvc_py_tools/align_reads.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python3
+import argparse
+from pvc_tools import *
+from pathlib import Path
+
+
+def align_main():
+    parser = argparse.ArgumentParser(description='Align reads to a PanVC-indexed pan-genome.')
+    parser.add_argument("--pgindex_dir",          required=True, help="PanVC index directory")
+    parser.add_argument("-r1", "--reads_file_1",  required=True, help="reads file 1 (aka)    ")
+    parser.add_argument("-r2", "--reads_file_2",  default="",    help="reads file 2 (optional)")
+    parser.add_argument("-o",  "--sam_output_dir", required=True, help="Output folder to store variant calling results")
+
+    parser.add_argument("--debug", action='store_true', help="Will run extra checks, degrading performance.")
+    parser.add_argument("-t", "--n_threads",  type=int, default=1,    help="Number of threads")
+    ## TODO: check if we really want this to be a parameter HERE. (it will be for the index though.)
+    parser.add_argument("-d", "--max_edit_distance",  type=int, default=10,    help="Max edit distance")
+    parser.add_argument("-p", "--ploidity",  type=int, default=2,    help="Ploidity of the genomes")
+    args = parser.parse_args()
+    PVC_align(args)
+
+def PVC_align(args):
+    pgindex_dir = args.pgindex_dir;
+    reads_1 = args.reads_file_1
+    reads_2 = args.reads_file_2
+    output_folder = args.sam_output_dir
+
+    paired_flag = (args.reads_file_2 != "")
+
+    debug_mode = args.debug
+    n_threads = args.n_threads
+    max_edit_distance = args.max_edit_distance
+    ploidity = args.ploidity
+
+    ensure_dir(output_folder)
+
+    reads_all = reads_1
+    if (paired_flag):
+        reads_all = PVC_merge_reads(reads_1, reads_2)
+
+    n_refs = PVC_nrefs_from_index(pgindex_dir)
+    read_len = PVC_read_len_from_reads(reads_all)
+    read_len_file = output_folder + "/read_len.txt"
+    with open(read_len_file, 'w') as f:
+        f.write(str(read_len))
+
+    chr_list_file = pgindex_dir + "/chr_list.txt"
+    assert(os.path.isfile(chr_list_file))
+
+
+    print ("Reference contains : " + str(n_refs) + " references")
+    print ("Read len           : " + str(read_len))
+    ##TODO:  do I want the old "reuse sam option?"
+
+    sequence_all_file = pgindex_dir + "/recombinant.all.fa"
+
+    chic_align_bin = os.environ["CHIC_ALIGN_BIN"]
+    chic_align_flags ="--secondary=LZ --threads=" + str(n_threads) + " --kernel-options=--n-ceil=C," + str(max_edit_distance) + ",0"
+    alignment_command = ""
+    if debug_mode:
+        sam_all_plain = output_folder + "/mapped_reads_all.sam"
+        alignment_command = chic_align_bin + " " + chic_align_flags + " " +sequence_all_file + " " + reads_all + " --output="+ sam_all_plain 
+        samtools_view_command = "${SAMTOOLS} view -Sb " + sam_all_plain + " > " + output_folder + "/all_mapped.bam"
+        call_or_die(alignment_command)
+        call_or_die(samtools_view_command)
+    else:
+        alignment_command = chic_align_bin + " " + chic_align_flags + " " +sequence_all_file + " " + reads_all +" | ${SAMTOOLS} view -Sb -  > " + output_folder + "/all_mapped.bam"
+        call_or_die(alignment_command)
+
+    sort_command ="${SAMTOOLS} sort -@ " + str(n_threads) + " " + output_folder+"/all_mapped.bam" + " " + output_folder + "/all_sorted"
+    call_or_die(sort_command)
+
+    split_command="${BAMTOOLS} split -reference -in "+output_folder+"/all_sorted.bam"
+    call_or_die(split_command)
+
+    with open(chr_list_file) as f:
+        for line in f:
+            chr_id = int(line)
+            print ("processing chr: " + str(chr_id))
+            samples_name_file = pgindex_dir + "/" + str(chr_id) + "/names.plain"
+            Path(output_folder + "/" + str(chr_id)).mkdir()
+
+            for curr_ref in range(1,n_refs+1):
+                print ("Ref id: " + str(curr_ref))
+                curr_fasta_name = PVC_sequence_num_to_name(samples_name_file, chr_list_file, ploidity, chr_id, curr_ref)
+                bam_file = output_folder + "/all_sorted.REF_" + curr_fasta_name  + ".bam"
+                sam_file = output_folder + "/" + str(chr_id)  + "/mapped_reads_to" + str(curr_ref) + ".sam.gz"
+                if not os.path.isfile(bam_file):
+                    assert not os.path.exists(sam_file)
+                    Path(sam_file).touch()
+                else:
+                    command = "${SAMTOOLS} view -@ " + str(n_threads) + " " + bam_file + " | gzip > " + sam_file
+                    call_or_die(command)
+
+if __name__ == "__main__":
+    align_main()
+
---
+++ pvc_py_tools/pvc_tools.py
@@ -0,0 +1,159 @@
+import os.path
+import sys
+import gzip
+from pathlib import Path
+#from config import *
+def PVC_make_std_ref(args):
+    output_file = args.output_dir + "/std_ref.fa"
+    out_file = open(output_file, "w")
+    with open(args.chr_list_file) as f:
+        for line in f:
+            chr_id = str(int(line))
+            print ("processing chr: " + chr_id)
+            out_file.write(">" + chr_id + "\n")
+            src_file = args.output_dir + "/" + chr_id + "/recombinant.n1.gapped" 
+            assert(Path(src_file).is_file())
+            with open(src_file) as g:
+                for seq in g:
+                    out_file.write(seq.replace("-","").rstrip() + "\n")
+
+    
+def PVC_read_len_from_reads(reads_file):
+    print ("Read len from: " + reads_file)
+    file_path, file_extension = os.path.splitext(reads_file)
+    read_len = 0
+    if file_extension == ".gz":
+        with gzip.open(reads_file, 'r') as myfile:
+            a_line = myfile.readline()
+            a_read = myfile.readline().decode().rstrip('\n')
+    else:
+        with open(reads_file, 'r') as myfile:
+            a_line = myfile.readline()
+            a_read = myfile.readline().rstrip('\n')
+    read_len =len(a_read)
+    #TODO: save it
+    return read_len
+
+def PVC_nrefs_from_index(pgindex_dir):
+    n_refs_file = pgindex_dir + "/n_refs.txt"
+    assert(os.path.isfile(n_refs_file))
+    with open(n_refs_file, 'r') as myfile:
+        ans = myfile.read()
+    return int(ans)
+
+def PVC_validate_names(pgindex_dir):
+    #TODO: implement me
+    """
+    F_CHR=$( head -n1 ${CHR_LIST_FILE})
+    if [ ${DEBUG_MODE} -eq 1 ]; then
+    cat ${CHR_LIST_FILE} | while read CHR_ID
+    do
+      utils_assert_files_are_equal ${PG_INDEX_OUTPUT_DIR}/${F_CHR}/n_refs.txt ${PG_INDEX_OUTPUT_DIR}/${CHR_ID}/n_refs.txt
+      utils_assert_namefiles_are_almost_equal ${PG_INDEX_OUTPUT_DIR}/${F_CHR}/names.plain ${PG_INDEX_OUTPUT_DIR}/${CHR_ID}/names.plain
+    done
+    fi
+    """
+    pass
+
+#TODO: receive output dir as well, we do not want this file to lie in the input folder, which is the current behavior
+def PVC_merge_reads(reads_1, reads_2):
+    ## assert reads_1, 2, are valid files.
+    basedir = os.path.dirname(reads_1)
+    output = basedir + "/reads_ALL_RENAMED.fq.gz"
+    if os.path.isfile(output):
+        return output
+    file_path, file_extension = os.path.splitext(reads_1)
+    bash_command = ""
+    if file_extension == ".gz":
+        bash_command = "awk '{if ((NR-1)%4==0) print \"@\"1+(NR-1)/4; else print }' <(zcat "+ reads_1 +") <(zcat " + reads_2 + ") | gzip > " + output
+    else:
+        bash_command = "awk '{if ((NR-1)%4==0) print \"@\"1+(NR-1)/4; else print }' " + reads_1 + " " + reads_2 + " | gzip > " + output
+    call_or_die(bash_command)
+    print ("Done")
+    return output
+
+def PVC_sequence_num_to_name(SAMPLES_FILENAME, CHRS_FILENAME, PLOIDITY, CHR_ID, SEQ_ID):
+    assert (SEQ_ID >= 1)
+    hap_suffix = ""
+    if (SEQ_ID != 1) :
+        offset = SEQ_ID - 2
+        target_line = 1 + (offset//PLOIDITY)
+        hap_suffix = "_" + str(offset % PLOIDITY)
+    else:
+        target_line = 0
+        hap_suffix = ""
+
+    with open(SAMPLES_FILENAME, 'r') as f:
+        curr_name = f.readline()
+
+    # Open and read file into buffer
+    f = open(SAMPLES_FILENAME,"r")
+    sample_names = f.readlines()
+
+    total_chroms = sum(1 for line in open(CHRS_FILENAME))
+    
+    chr_suffix = ""
+    if (total_chroms != 1) :
+        chr_suffix="_CHR"+str(CHR_ID)
+
+    #we are currently deleting whitespaces from the samples names;
+    #if a replacement is needed just change the next line.
+    SPACEREPLACEMENT=""
+    #answer="PG_REF_"+str(SEQ_ID)+"_CHR"+str(CHR_ID)
+    answer = sample_names[target_line].strip().replace(" ", SPACEREPLACEMENT)+hap_suffix+chr_suffix
+    return answer
+
+def PVC_splitA2M(A2M_FILENAME, target_folder):
+    n_refs = 0
+    f = open(A2M_FILENAME,"r")
+    out_num = 0
+    for line in f:
+        if ";" in line :
+            continue
+        if ">" in line :
+            out_num = out_num + 1
+            #close?
+            out_file = open(target_folder + '/recombinant.n'+str(out_num)+'.gapped', 'w')
+            continue
+        out_file.write(line)
+        n_refs = n_refs + 1
+    return n_refs
+
+def PVC_gapped_len(filename):
+    f = open(filename, "r")
+    line = f.readline().rstrip()
+    return len(line)
+
+# what about newlines? shall I remove them?
+
+def ensure_dir(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+def ensure_clean_dir(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    else:
+        import shutil
+        shutil.rmtree(directory)
+        os.makedirs(directory)
+
+def call_or_die(command):
+    import subprocess
+    print ("About to call:")
+    print (command)
+    print ("--------------")
+    ret_code = subprocess.call([command], shell=True, executable="/bin/bash")
+    if ret_code != 0:
+        print ("Return code not zero:" + str(ret_code))
+        sys.exit(ret_code)
+
+def file_exists_or_die(filename):
+    if not os.path.isfile(filename):
+        print("******")
+        print("Cannot find " + filename)
+        print("Aborting execution")
+        print("******")
+        sys.exit(1);
+
+
