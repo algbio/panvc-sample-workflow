e5d435875998b5bf16ef1e0f60d46ef0a2438bc1
Tuukka Norri <tsnorri@iki.fi>
2020-10-26 22:06:22 +0200

    Fix splitting aligned reads

--- pvc_py_tools/align_reads.py
+++ pvc_py_tools/align_reads.py
@@ -8,6 +8,7 @@ import distutils.spawn as ds
 import glob
 import os
 import re
+import sys
 from pvc_tools import PVC_save_var, PVC_read_len_from_reads, call_or_die
 from pathlib import Path
 
@@ -41,16 +42,12 @@ def run_panvc_aligner(reads_all, pgindex_dir, chr_list, ploidy, n_refs, max_read
         os.remove(unsorted_bam)
 
     for chr_id in chr_list:
-        samples_name_file = pgindex_dir + "/" + chr_id + "/names.plain"
-        #Path(output_folder + "/" + chr_id).mkdir()
-
-        for curr_ref in range(1,n_refs+1):
-            curr_fasta_name = str(curr_ref) #PVC_sequence_num_to_name(samples_name_file, len(chr_list), ploidy, chr_id, curr_ref)
-            bam_file = output_folder + "/all_sorted.REF_" + curr_fasta_name  + ".bam"
-            sam_file = output_folder + "/" + chr_id  + "/mapped_reads_to" + str(curr_ref) + ".sam.gz"
+        for curr_ref in range(1, n_refs + 1):
+            input_bam = f"{output_folder}/all_sorted.REF_{chr_id}_{curr_ref}.bam"
+            output_sam = f"{output_folder}/{chr_id}/mapped_reads_to{curr_ref}.sam.gz"
             if not os.path.isfile(bam_file):
                 assert not os.path.exists(sam_file)
                 Path(sam_file).touch()
             else:
-                command = "samtools view -@ " + str(n_threads) + " " + bam_file + " | gzip > " + sam_file
+                command = f"samtools view -@ {n_threads} {input_bam} | gzip > {output_sam}"
                 call_or_die(command)
