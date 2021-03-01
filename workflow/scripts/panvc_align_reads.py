96a940a09f877ae695c0d7da20f91a82daa6c201
Tuukka Norri <tsnorri@iki.fi>
2021-03-01 22:49:57 +0200

    Update PanVC

--- workflow/scripts/panvc_align_reads.py
+++ workflow/scripts/panvc_align_reads.py
@@ -1,21 +1,30 @@
-# Copyright (c) Tuukka Norri 2020
+# Copyright (c) Tuukka Norri 2020-2021
 # Licenced under the MIT licence.
 
 
 import sys
+import distutils.spawn as ds
 
-sys.path.append(f"{snakemake.scriptdir}/../../pvc_py_tools")
-from align_reads import run_panvc_aligner
+sys.path.append(f"{snakemake.params.basedir}/pvc_py_tools")
+from pvc_tools import PVC_read_len_from_reads
 
-sam_output_dir = f"{snakemake.params.output_root}/sam_files"
-run_panvc_aligner(
-	snakemake.input.reads_all,
-	snakemake.params.index_root,
-	snakemake.params.chromosome_list,
-	snakemake.params.ploidy,
-	snakemake.params.n_refs,
-	snakemake.params.max_read_len,
-	snakemake.params.max_edit_distance,
-	snakemake.threads,
-	sam_output_dir
-)
+read_len = PVC_read_len_from_reads(snakemake.input.reads_all)
+assert(read_len <= snakemake.params.max_read_len)
+
+samtools_path = ds.find_executable("samtools")
+
+import snakemake.shell as shell # If imported earlier, the module masks the global variable “snakemake”.
+shell((
+	"mkdir -p {snakemake.output} &&"
+	" chic-align"
+	" --secondary-report=NONE"
+	" --threads={snakemake.threads}"
+	" --kernel-options=--n-ceil=C,{snakemake.params.max_edit_distance},0"
+	" --max-ed={snakemake.params.max_edit_distance}"
+	" --split-output-by-reference"
+	" --output-prefix={snakemake.output}"
+	" --samtools-path={samtools_path}"
+	" --no-samtools-threads"
+	" --verbose=3"
+	" {snakemake.input.recombinant_all} {snakemake.input.reads_all}"
+))
